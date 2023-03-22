1d6fb69d91871d11cfd97e218942752a6aed8b76
TheperZ <ckdgh7884@gmail.com>
2023-03-22 23:51:42 +0900

    add practice proj

---
+++ CH_Practice/src/main/java/com/example/CH_Practice/ChPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CH_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChPracticeApplication.class, args);
+	}
+
+}
