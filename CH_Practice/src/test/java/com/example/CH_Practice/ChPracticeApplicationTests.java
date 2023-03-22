7dd73854cff28f0472774f40f67e43eb35068080
TheperZ <ckdgh7884@gmail.com>
2023-03-23 00:06:02 +0900

    add practice proj

---
+++ CH_Practice/src/test/java/com/example/CH_Practice/ChPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CH_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
