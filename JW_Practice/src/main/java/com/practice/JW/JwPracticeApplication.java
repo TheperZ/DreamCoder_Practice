6c0e142c53d1df11151bd9ac881fbad075ce3114
AshRainK <kjo980430@gmail.com>
2023-03-22 21:12:14 +0900

    Spring Boot Initializar

---
+++ JW_Practice/src/main/java/com/practice/JW/JwPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.JW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwPracticeApplication.class, args);
+	}
+
+}
