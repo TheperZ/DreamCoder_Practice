6440e49680b9ad73d5545d444be8d0a27cbd86b5
Changzin <changzin0126@gmail.com>
2023-03-22 17:44:46 +0900

    개인 프로젝트 폴더 추가

---
+++ Changjin_Practice/src/main/java/DreamCoder/Changjin_Practice/ChangjinPracticeApplication.java
@@ -0,0 +1,13 @@
+package DreamCoder.Changjin_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangjinPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangjinPracticeApplication.class, args);
+	}
+
+}
