85d0dbd9597f94bae3b15c50825bd86701b55a04
changzin <changzin0126@gmail.com>
2023-03-25 12:03:57 +0900

    GET 메소드를 사용한 CRUD 구현

--- Changjin_Practice/src/main/java/DreamCoder/Changjin_Practice/controller/UserController.java
+++ Changjin_Practice/src/main/java/DreamCoder/Changjin_Practice/controller/UserController.java
@@ -26,16 +26,49 @@ public class UserController {
         user.setUserName(userRequestDto.getUserName());
         user.setEmail(userRequestDto.getEmail());
         user.setPassword(userRequestDto.getPassword());
-        userService.join(user);
-
-        return ResponseEntity.ok().body(userService.findOne(user.getUserId()).get());
+        try{
+            userService.join(user);
+        }
+        catch(IllegalStateException e){
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body(userService.findOne(user.getUserId()));
     }
     @GetMapping("/users/find")
     public ResponseEntity read(@RequestParam("userId") Integer userId){
-        Optional<User> findUser = userService.findOne(userId);
-        if (findUser.isPresent())
-            return ResponseEntity.ok().body(findUser);
-        else
-            return new ResponseEntity<>("찾을 수 없는 유저", HttpStatus.OK);
+        User findUser;
+        try{
+            findUser = userService.findOne(userId);
+        }
+        catch(IllegalStateException e){
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body(findUser);
+    }
+    @GetMapping("/users/update")
+    public ResponseEntity update(@RequestParam("userId") Integer userId, UserRequestDto userRequestDto){
+        User findUser;
+        User updateUser = new User();
+        updateUser.setUserName(userRequestDto.getUserName());
+        updateUser.setEmail(userRequestDto.getEmail());
+        updateUser.setPassword(userRequestDto.getPassword());
+        try{
+            findUser = userService.findOne(userId);
+        }
+        catch(IllegalStateException e){
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body(userService.updateOne(userId, updateUser));
+    }
+    @GetMapping("/users/delete")
+    public ResponseEntity delete(@RequestParam("userId") Integer userId, UserRequestDto userRequestDto){
+        User findUser;
+        try{
+            findUser = userService.findOne(userId);
+        }
+        catch(IllegalStateException e){
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body(userService.deleteOne(userId));
     }
 }
